dfed493cd2462cfd415cc10e4265e9b5e4ff9354
ert224 <erick.tepan2001@gmail.com>
2023-03-17 13:46:24 -0400

    "Resume page completed"

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,36 +1,28 @@
-import Loader from 'react-loaders';
 import './Resume.scss';
-import React from 'react';
-import {useState} from 'react';
-// import 'bootstrap/dist/css/boootstrap.min.css';
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import pdfFile from './Tepan_Erick_Resume.pdf';
 
-import {Viewer, Worker} from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import '@react-pdf-viewer/core/lib/styles/index.css'
-import '@react-pdf-viewer/default-layout/lib/styles/index.css'
-import myPDF from './Tepan_Erick_Resume.pdf'
 const Resume = () => {
-    const viewPDF = myPDF;
-    const newplugin = defaultLayoutPlugin();
-    return (
-        <div className="container resume-page">
-        <div className="text-zone">
-            <h1> R E S U M E</h1>
-        </div>
-        <div className='pdf-container'>
-                <Worker workerUrl = "https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
-                    {
-                        Viewer && <>
-                            <Viewer fileUrl = {viewPDF} plugins= {[newplugin]}/>
-                        </>
-                    }
-                    {
-                        !viewPDF && <>No PDF</>
-                    }
-                </Worker>
-            </div>
-        </div>
-    );
-}
+  const [numPages, setNumPages] = useState(null);
 
-export default Resume
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+   
+    <div className="container resume-page">
+      <div className="pdf_page">
+      <h1>R E S U M E</h1>
+        <Document file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
+          {Array.from(new Array(numPages), (_, index) => (
+            <Page key={`page_${index + 1}`} pageNumber={index + 1} renderTextLayer={false} />
+          ))}
+        </Document>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
