fc3414ae9db29a09f069baf1382bb1e8394e915d
ert224 <erick.tepan2001@gmail.com>
2023-03-18 17:18:32 -0400

    "Resume iframe"

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -15,19 +15,19 @@ const Resume = () => {
     <div className="container resume-page">
       <div className="pdf_page">
         <h1>R E S U M E</h1>
-      
-        <a className='pdf_link'
+        {/* <a className='pdf_link'
           href="https://drive.google.com/file/d/1l61d1rolfAsDIVcoPzicc4xpxMI7L0FU/view?usp=share_link" 
           target="_blank" rel="noopener noreferrer">
           Click to Print
-        </a>
-        
-        <div className="pdf-container">
-          <Document file={pdfFile} onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.from(new Array(numPages), (_, index) => (
-              <Page key={`page_${index + 1}`} pageNumber={index + 1} renderTextLayer={false} />
-            ))}
-          </Document>
+        </a> */}
+        <div>
+          <iframe 
+          src="https://drive.google.com/file/d/1l61d1rolfAsDIVcoPzicc4xpxMI7L0FU/preview" 
+          width="800" 
+          height="900" 
+          allow="autoplay">
+            
+          </iframe>
         </div>
       </div>
       <Loader type="pacman" />
